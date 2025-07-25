1dac237e6b77fce3c2ae195416934a6d96e3a77e
skovtimur <timurskovorodnikov10@gmail.com>
2025-07-25 11:58:07 +0500

    updated-readme

--- AccountService/Requests/WalletCreateRequest.cs
+++ AccountService/Requests/WalletCreateRequest.cs
@@ -8,7 +8,7 @@ namespace AccountService.Requests;
 public class WalletCreateRequest
 {
     [Required] public Guid OwnerId { get; set; }
-    [Required]  public WalletType Type { get; set; }
+    [Required] public WalletType Type { get; set; }
 
 
     [Required, StringLength(maximumLength: 3, MinimumLength = 2)]
@@ -16,6 +16,6 @@ public class WalletCreateRequest
 
     [Range(0, 100)] public decimal? InterestRate { get; set; }
     [Required, Range(0, int.MaxValue)] public decimal Balance { get; set; }
-    
+
     public DateTime? ClosedAtUtc { get; set; }
 }
